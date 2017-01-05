e28e94eda48cb81d04ea812f512db80c0b7ee90c
yugandhar1452 <yugandhar0458@hotmail.com>
2017-01-05 17:50:07 +0530

    Add files via upload

--- Pratice1.java
+++ Pratice1.java
@@ -1,5 +1,6 @@
 package SeleniumVelocity;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -9,8 +10,10 @@ import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class Pratice1 {
+
 
 	static WebDriver driver=null;
 
@@ -18,7 +21,7 @@ public class Pratice1 {
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		js.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "color: red; border: 10px solid red;");
 		js.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "");
-		driver.manage().timeouts().Explicitwait(100, TimeUnit.SECONDS);
+		//		driver.manage().timeouts().Explicitwait(100, TimeUnit.SECONDS);
 	}
 
 	/*public void setHighlight(WebElement element)
@@ -51,38 +54,57 @@ public class Pratice1 {
 		String chrome="E:\\Selenium Practice\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", chrome);
 		driver = new ChromeDriver();
+		//Login into application
 		driver.get("https://qa.velocity.voyagersopris.com/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		Pratice1.highlightElement(driver.findElement(By.id("inputEmail")));
 		driver.findElement(By.id("inputEmail")).sendKeys("zengrade4z3");
 		driver.findElement(By.id("inputPassword")).sendKeys("zengrade4z3");
 		driver.findElement(By.xpath("//form[@id='k5i-login-form']/input[3]")).click();
+
+		//verification of loadina bars
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		String value1 =driver.findElement(By.className("loader")).getText();
 		if(value1!=null)
 			System.out.println("Loading Bar is available");
 		else{System.out.println("Loading bar not available");}
+
+		//verification of dash character
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		String value2 =driver.findElement(By.className("spine-canvas")).getText();
 		if(value2!=null)
 			System.out.println("Dash is available");
 		else{System.out.println("Dash is not available");}
+
+		//verification of World selector
 		String value3 =driver.findElement(By.className("vsl_velocity_views_student_dashboard-worlds-label")).getText();
 		if(value3!=null)
 			System.out.println("World selecor is available");
 		else{System.out.println("World selector not available");}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		driver.get("https://qa.velocity.voyagersopris.com/scene");
+
+		//take a list of drop down elements 
+		WebElement LessonType=driver.findElement(By.id("lessonType"));
+		Select LessontypeDP=new Select(LessonType);
+		List<WebElement> ProblemTypes = LessontypeDP.getOptions();
+		System.out.println(ProblemTypes.size());
+		for(WebElement Problemtype:ProblemTypes)
+		{
+			for(int i=0;i<=ProblemTypes.size();i++)
+			{
+				
+				LessontypeDP.selectByIndex(i);
+				System.out.println(Problemtype.getText());
+				driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+				WebElement Lessonname=driver.findElement(By.id("lesson"));
+				Select LessonDP=new Select(Lessonname);
+				List<WebElement> Lessonnames=LessonDP.getOptions();
+				for(WebElement Lesson:Lessonnames)
+					System.out.println(Lesson.getText());
+			}
+		}
+
+
 	}
 
 }
